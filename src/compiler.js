e8f8da17339e743845e356b1deff0d707185ff70
thfabricio <thiago.fabricio123@gmail.com>
2024-12-15 12:49:33 -0300

    feat: automatizacao da leitura dos arquivos a serem processados

--- src/compiler.js
+++ src/compiler.js
@@ -8,21 +8,40 @@ const r1 = readline.createInterface({
     output: process.stdout,
 });
 
+// Ler todos os arquivos da pasta "tests"
+const dirPath = './tests';
+const files = fs.readdirSync(dirPath).filter(file => fs.statSync(`${dirPath}/${file}`).isFile());
+
+// Exibir a lista de arquivos com índice
+console.log("Arquivos disponíveis:");
+files.forEach((file, index) => {
+    console.log(`${index + 1} - ${file}`);
+});
+
 const filepath = '.\tests\test2.txt'
 
-r1.question("Enter the path of the file: ", (filepath) => {
+r1.question("Digite o número do arquivo que deseja processar: ", (choice) => {
     try {
+        const index = parseInt(choice, 10);
+        if (isNaN(index) || index < 1 || index > files.length) {
+            console.log("Opção inválida.");
+            r1.close();
+            return;
+        }
+
+        const filepath = `${dirPath}/${files[index - 1]}`;
+
+        // Lê o conteúdo do arquivo selecionado
         const input = fs.readFileSync(filepath, "utf8");
         const lexer = new Lexer(input);
         lexer.tokenize();
 
-        let tokens = lexer.tokensList
+        let tokens = lexer.tokensList;
+        const parser = new Parser(tokens);
 
-        const parser = new Parser(tokens)
+        const ast = parser.parse();
 
-        const ast = parser.parse()
-
-        const astJson = JSON.stringify(ast, null, 2)
+        const astJson = JSON.stringify(ast, null, 2);
 
         // Salva os tokens no arquivo de saída
         const tokensJson = JSON.stringify(lexer.tokensList, null, 2);
@@ -36,14 +55,14 @@ r1.question("Enter the path of the file: ", (filepath) => {
 
         // Exibe a saída do parser (AST)
         console.log("\nAST gerada:");
-        console.log(JSON.stringify(ast, null, 2));
+        console.log(astJson);
     } catch (error) {
-        console.log("File not found ", + filepath);
-        console.log(error)
+        console.log("Erro ao processar o arquivo.");
+        console.log(error);
     } finally {
         r1.close();
     }
-})
+});
 // const code = `
 //     var = 10;
 //     if (x > 3) {
